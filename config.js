758508ea8bbe5d720583e8b805b53ed6a30c6cff
BoitumeloMo <93591675+BoitumeloMo@users.noreply.github.com>
2022-09-22 10:03:51 +0200

    corrections

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 
 
-let roothPath = "https://mysite.itvarsity.org/api/ContactBook/";
+let rootPath ="https://mysite.itvarsity.org/api/ContactBook/";   // location of the server
 let apiKey = checkApiKey();
 
-function checkApiKey(){
-    if(!localStorage.getItem("apiKey")) {
-        window.open("enter-api-key.html","_self");
+function checkApiKey() {    // sends the user to another page if their api key is not entered
+    if (!localStorage.getItem("apiKey")) {
+        window.open("enter-api-key.html", "_self");
     }
     return localStorage.getItem("apiKey");
 }
